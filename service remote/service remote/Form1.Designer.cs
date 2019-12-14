7012e11b082b7bb5b3d43c19a4a5367ebf28bca2
Aavesh Jilani <50949867+aaveshdev@users.noreply.github.com>
2019-12-15 02:05:30 +0530

    Add files via upload

---
+++ service remote/service remote/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace service_remote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(180, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Share my screen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 381);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ service remote/service remote/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Drawing.Imaging;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Reflection;
+using System.IO;
+
+namespace service_remote
+{
+    public partial class Form1 : Form
+    {
+        private readonly TcpClient client = new TcpClient();
+        private NetworkStream mainstream;
+        private int port;
+        private string ips;
+        private static Image GrabDesktop()
+        {
+            Rectangle bounds = Screen.PrimaryScreen.Bounds;
+            Bitmap screenshot = new Bitmap(bounds.Width, bounds.Height, PixelFormat.Format32bppArgb);
+            try
+            {
+                //Realtime Screenshot capture
+                Graphics graphic = Graphics.FromImage(screenshot);
+                graphic.CopyFromScreen(bounds.X, bounds.Y, 0, 0, bounds.Size, CopyPixelOperation.SourceCopy);
+            }
+            catch (Exception op)
+            {
+				//In case application crash then automatically restarts.
+                Application.Restart();
+            
+            }
+            return screenshot;
+
+        
+        }
+
+
+        private void sendDesktopImage()
+        {
+            BinaryFormatter binary = new BinaryFormatter();
+            while (client.Connected)
+            {
+                mainstream = client.GetStream();
+                try
+                {
+					//sending the Captured data.
+                    binary.Serialize(mainstream, GrabDesktop());
+                }
+                catch (Exception eo)
+                {
+
+                    Application.Restart();
+                }
+            }
+           
+
+        
+        
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conc();
+            button2.Text = "Stop Sharing";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conc();
+        }
+        private void jonc()
+        {
+            timer1.Stop();
+            conc();
+        
+        }
+
+        private void conc()
+        {
+          //  port = int.Parse(textBox2.Text);
+            try
+            {
+                string ap = Assembly.GetExecutingAssembly().Location;
+                string ico = "";
+				//Reading the IP and Port from configuration file.
+                ico = System.Text.RegularExpressions.Regex.Replace(ap, @"service remote.exe$", "conf.txt");
+                string source1 = File.ReadAllText(@ico);
+                string[] arr = source1.Split(' ');
+                ips = arr[0];
+                port = int.Parse(arr[1]);
+                client.Connect(ips, port);
+               // MessageBox.Show("Connected");
+                timer1.Start();
+            }
+            catch (Exception ec)
+            {
+               // MessageBox.Show("Something went wrong");
+
+                jonc();
+            }
+        
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+                timer1.Start();
+                button2.Text = "Stop Sharing";
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+			//sending image in time event.
+            sendDesktopImage();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button2.Text = "Share my screen";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
